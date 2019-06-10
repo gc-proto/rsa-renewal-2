7ea51f0923f3f0f59621c63eaeda7f5e240815eb
Borges-Porteous <miguel.borges-porteous@tbs-sct.gc.ca>
2019-06-10 10:59:41 -0400

    Removing gyro and getting latest version in pageLoadMore

--- GCWeb/js/pageLoadMore.js
+++ GCWeb/js/pageLoadMore.js
@@ -1,4 +1,4 @@
-/*! dataTables.pageLoadMore.js 1.0.0
+/*! dataTables.pageLoadMore.js 1.0.1
  *  Copyright (c) Gyrocode LLC (www.gyrocode.com)
  *  License: MIT License
  */
@@ -12,7 +12,7 @@
         var cacheLastRequest = null;
         var cacheLastJson = null;
         return function(request, drawCallback, settings) {
-            if (!settings.pageLoadMore) {
+            if (!settings.hasOwnProperty("pageLoadMore")) {
                 var api = new $.fn.dataTable.Api(settings);
                 var info = api.page.info();
                 settings.pageLoadMore = {
@@ -60,7 +60,10 @@
     };
     $.fn.dataTable.Api.register("page.resetMore()", function() {
         return this.iterator("table", function(settings) {
-            api.page.len(settings.pageLoadMore.pageLength);
+            var api = this;
+            if (settings.hasOwnProperty("pageLoadMore")) {
+                api.page.len(settings.pageLoadMore.pageLength);
+            }
         });
     });
     $.fn.dataTable.Api.register("page.hasMore()", function() {
@@ -73,7 +76,7 @@
             var api = this;
             var info = api.page.info();
             if (info.pages > 1) {
-                if (!settings.pageLoadMore) {
+                if (!settings.hasOwnProperty("pageLoadMore")) {
                     settings.pageLoadMore = {
                         pageLength: info.length
                     };
